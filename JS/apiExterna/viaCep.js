0e953173bef5bdf86e538a17e0a71f630856e8f8
devdudd <tybuscheduarda@gmail.com>
2022-04-25 17:19:03 -0300

    API cep consumindo dados, interface finalizada

--- JS/apiExterna/viaCep.js
+++ JS/apiExterna/viaCep.js
@@ -11,3 +11,27 @@ class ViaCep {
 
   }
 }
+const preencherFormulario = (endereco) => {
+  document.querySelector('#rua').value = endereco.logradouro
+  document.querySelector('#bairro').value = endereco.bairro
+  document.querySelector('#cidade').value = endereco.localidade
+  document.querySelector('#estado').value = endereco.uf
+
+}
+const cepValido = (cep) => cep.length == 8 
+const pesquisaCep = async () => {
+  const cep = document.querySelector('#cep').value
+  const url = "https://viacep.com.br/ws/" + cep + "/json"
+  const dados = await fetch(url)
+  const endereco = await dados.json()
+  if (cepValido(cep)) {
+    if (endereco.hasOwnProperty('erro')) {
+      alert('Cep inválido');
+    } else {
+      preencherFormulario(endereco)
+    }
+  } else {
+    alert ('CEP incorreto!')
+  }
+}
+document.querySelector('#cep').addEventListener('focusout', pesquisaCep)
